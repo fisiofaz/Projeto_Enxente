0049fcfbc7ce438731f478d4fc2886ba7113d9a3
fisiofaz <75371386+fisiofaz@users.noreply.github.com>
2024-10-26 11:01:27 -0400

    refatore e style: html e css index

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let currentNews = 0;
+    const news = [
+        "Alerta de enchente na região Sul",
+        "Medidas preventivas contra enchentes",
+        "Como agir em caso de enchentes"
+    ];
+    const newsDisplay = document.getElementById('newsDisplay');
+    
+    function updateNews() {
+        newsDisplay.innerText = news[currentNews];
+        currentNews = (currentNews + 1) % news.length;
+    }
+    
+    setInterval(updateNews, 3000); // Atualiza a cada 3 segundos
+});
+
+function scrollToCategories() {
+    document.getElementById('categorias').scrollIntoView({ behavior: 'smooth' });
+}
