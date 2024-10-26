870d1d677cffa2efd983bd7bd4d32a214fd3ffc3
PCSTEPHANIE <stephaniecodato.b@gmail.com>
2024-10-26 19:33:31 -0300

    feat: alteracoes stephanie

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,3 +27,25 @@ function showFundamento() {
         fundamentoText.style.display = "none";
     }
 }
+
+   // Seleciona todas as perguntas da FAQ
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+// Adiciona um evento de clique a cada pergunta
+faqQuestions.forEach(question => {
+    question.addEventListener('click', () => {
+        // Alterna a classe 'active' para a pergunta clicada
+        question.classList.toggle('active');
+
+        // Seleciona a resposta correspondente
+        const answer = question.nextElementSibling;
+
+        // Verifica se a resposta está visível
+        if (answer.style.display === "block") {
+            answer.style.display = "none"; // Oculta a resposta
+        } else {
+            answer.style.display = "block"; // Mostra a resposta
+        }
+    });
+});
+
